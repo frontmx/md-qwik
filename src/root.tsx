2b96e668185add40ad1a6cff563c55bb33374db5
Iván Hernández <honter1997@gmail.com>
2023-09-25 22:04:46 -0600

    feat: almost extended-fab-button, ripple missing

--- src/root.tsx
+++ src/root.tsx
@@ -1,6 +1,7 @@
 import {OutlinedButton} from "./components/outlined-button/outlined-button";
 import "./global.css";
-
+import ExtendedFabButton from './components/extended-fab-button/extended-fab-button';
+import './temp/material/theme.css'
 export default () => {
     return (
         <>
@@ -12,7 +13,11 @@ export default () => {
                 <link rel="preconnect" href="https://fonts.gstatic.com"/>
                 <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"/>
             </head>
-            <body>
+            <body style={{
+              display: "flex",
+              gap: "5px",
+              flexDirection: "column"
+            }}>
             <div style="display: flex; gap: 16px">
                 <OutlinedButton>Enabled</OutlinedButton>
                 <OutlinedButton disabled>Disabled</OutlinedButton>
@@ -21,6 +26,14 @@ export default () => {
             <div style="display: flex; gap: 16px">
                 <OutlinedButton icon="add">Enabled</OutlinedButton>
                 <OutlinedButton icon="add" disabled>Disabled</OutlinedButton>
+            </div>
+            <div style="display: flex; gap: 16px">
+              <ExtendedFabButton icon="edit" color="primary" >Enabled with icon</ExtendedFabButton>
+              <ExtendedFabButton icon="edit" disabled color="primary" >Disabled with icon</ExtendedFabButton>
+              <ExtendedFabButton color="secondary" >Secondary</ExtendedFabButton>
+              <ExtendedFabButton color="tertiary" >Secondary</ExtendedFabButton>
+              <ExtendedFabButton color="error" >Error</ExtendedFabButton>
+             
             </div>
             </body>
         </>
